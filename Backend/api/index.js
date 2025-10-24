3b77b1944b1af94af3f3c5e32d44521b2f64fb2c
FSD9-Wave <loomaflex44@gmail.com>
2025-10-24 12:53:27 +0700

    Fix Express routing error: change '*' to '/*' for 404 handler

--- Backend/api/index.js
+++ Backend/api/index.js
@@ -68,7 +68,7 @@ app.use((err, req, res, next) => {
 });
 
 // 404 handler
-app.use('*', (req, res) => {
+app.use('/*', (req, res) => {
     res.status(404).json({ error: 'Route not found' });
 });
 
